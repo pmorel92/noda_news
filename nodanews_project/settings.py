8d1457be892a90ec0f5de00062d299918233cb5c
pmorel92 <philippe.a.b.morel@gmail.com>
2020-01-08 17:51:44 +0000

    static file

--- nodanews_project/settings.py
+++ nodanews_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.staticfiles',    
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'nodanews',
