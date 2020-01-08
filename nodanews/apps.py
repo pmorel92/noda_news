25dfe7769acb014ac016773998ac19beb32bf0b1
pmorel92 <philippe.a.b.morel@gmail.com>
2020-01-08 23:03:00 +0000

    django suit

--- nodanews/apps.py
+++ nodanews/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class NodanewsConfig(AppConfig):
     name = 'nodanews'
+    
+from suit.apps import DjangoSuitConfig
+
+
+class SuitConfig(DjangoSuitConfig):
+    layout = 'horizontal'
+    name = 'app_name'
