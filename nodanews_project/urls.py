a84f00656f77adf5ca9610148d377e92bfb396e0
pmorel92 <philippe.a.b.morel@gmail.com>
2020-01-08 23:24:17 +0000

    django suit

--- nodanews_project/urls.py
+++ nodanews_project/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import xadmin
+xadmin.autodiscover()
+
+# version模块自动注册需要版本控制的 Model
+from xadmin.plugins import xversion
+xversion.register_models()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tinymce/', include('tinymce.urls')),    
-#	path('', include('nodasf.urls')),
+    path('xadmin/', include(xadmin.site.urls),
 	path('', include('nodanews.urls')),	
 ]
