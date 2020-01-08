a13acabb4c9c931b67fb26dcde0ad8bfbe01f87c
pmorel92 <philippe.a.b.morel@gmail.com>
2020-01-08 19:15:59 +0000

    static file

--- nodanews/admin.py
+++ nodanews/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Event, Theme, Media_Org, Blog, Report_Link
+from .models import Event, Theme, Media_Org, Blog, Report_Link, Node, Media_Org, Link, Perspective, Node_Dir, Region, Journalist, Political_Lean, Media_Character, About, Topic_Link, PoliticalBiasNews, Blog, Analysis, AnalLink, AnalPerspective, PoliticalIssue, STF, STF_Hub, STF_Link, Feature, Feature_Link
 
 admin.site.register(Event)
 admin.site.register(Theme)
@@ -7,3 +7,26 @@ admin.site.register(Media_Org)
 admin.site.register(Blog)
 admin.site.register(Report_Link)
 
+####Archives###
+    
+
+admin.site.register(Node)
+admin.site.register(Node_Dir)
+admin.site.register(Region)
+admin.site.register(Link)
+admin.site.register(Perspective)
+admin.site.register(Political_Lean)
+admin.site.register(About)
+admin.site.register(Topic_Link)
+admin.site.register(Media_Character)
+admin.site.register(Journalist)
+admin.site.register(PoliticalBiasNews)
+admin.site.register(Analysis)
+admin.site.register(AnalLink)
+admin.site.register(AnalPerspective)
+admin.site.register(PoliticalIssue)
+admin.site.register(STF)
+admin.site.register(STF_Link)
+admin.site.register(STF_Hub)
+admin.site.register(Feature)
+admin.site.register(Feature_Link)
