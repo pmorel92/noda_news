d1138ff0cad74a34deaf153c93b3f0800a25b3c8
pmorel92 <philippe.a.b.morel@gmail.com>
2020-01-08 23:39:37 +0000

    uncomfortable

--- nodanews_project/urls.py
+++ nodanews_project/urls.py
@@ -15,15 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import xadmin
-xadmin.autodiscover()
-
-# version模块自动注册需要版本控制的 Model
-from xadmin.plugins import xversion
-xversion.register_models()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('xadmin/', include(xadmin.site.urls)),
 	path('', include('nodanews.urls')),	
 ]
