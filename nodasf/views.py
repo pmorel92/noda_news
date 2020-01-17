657933fbb6e9085cb245e8db6820ab14db3abd99
pmorel92 <philippe.a.b.morel@gmail.com>
2020-01-17 17:52:16 +0000

    here we go

--- nodasf/views.py
+++ nodasf/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.template.loader import get_template
+from django.template import Context
+from django.template.defaulttags import register
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F, Q
+from .models import Venue, Agency, Genre, Issue, Party, Level, Event, Politician, Local_Link, District, Category, Organization, Program, Bureaucrat, Story, Blog, Report_Link, Media_Org
 
-# Create your views here.
+def index(request):
+	return render (request, 'index.html')
+
+def indexUC(request):
+    
+    return render (request, 'indexUC.html')
